cb1a42f376619fa96ca38b5ba258861c30731484
Sirui Wang <siruignaw@gmail.com>
2026-09-04 19:30:32 -0600

    update README after comparing to neijenhuis' data

--- src/main.rs
+++ src/main.rs
@@ -76,7 +76,6 @@ fn main() {
         let n_neg = potentials.iter().filter(|&&v| v < 0.0).count();
         let n_pos = potentials.iter().filter(|&&v| v >= 0.0).count();
         let most_negative = potentials.iter().cloned().fold(0.0_f64, |a, b| a.min(b));
-        eprintln!("ph={}: n_neg={}, n_pos={}, most_negative_point={}", ph, n_neg, n_pos, most_negative);
 
         let surf_ep_neg_sum_val = surf_ep_neg_sum(&property_points, &charged_atoms);
         let row = format!("{},{},{},{}\n", id, ph, iso_point, surf_ep_neg_sum_val);
